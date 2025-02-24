981bd842003990dd7cbf564beaa34033e6d1206c
Peter Laca <peter@laca.me>
2025-02-24 11:53:57 +0100

    fix: hash

--- dotdev.rb
+++ dotdev.rb
@@ -2,7 +2,7 @@ class Dotdev < Formula
   desc "Lightweight Web server for static HTML with built-in live reload written in Go."
   homepage "https://github.com/petlack/dotdev"
   url "https://github.com/petlack/dotdev/archive/refs/tags/v0.0.1.20250224.03.tar.gz"
-  sha256 "f2e34ccbb16c2dd1433ca1c6b8e8320321da6559d2845fa7e81e7530f7e500b1"
+  sha256 "46075f612ab7ec05d84a8cc2061add0fecdb78545d57f1716fecb18a090611b7"
   license "GPL-3.0-or-later"
 
   depends_on "go" => :build
