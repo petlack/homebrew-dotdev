be812ca47312025e1f26d8fd4933e6955b03a8c9
Peter Laca <peter@laca.me>
2025-06-11 17:40:31 +0200

    release

--- dotdev.rb
+++ dotdev.rb
@@ -1,8 +1,8 @@
 class Dotdev < Formula
   desc "Lightweight Web server for static HTML with built-in live reload written in Go."
   homepage "https://github.com/petlack/dotdev"
-  url "https://github.com/petlack/dotdev/archive/refs/tags/v0.0.1.20250224.03.tar.gz"
-  sha256 "46075f612ab7ec05d84a8cc2061add0fecdb78545d57f1716fecb18a090611b7"
+  url "https://github.com/petlack/dotdev/archive/refs/tags/v0.0.1.20250611.01.tar.gz"
+  sha256 "ff54d1bded38c8e846c5218dd5d959e736d30fdbc0843023b744cdf1c1e97925"
   license "GPL-3.0-or-later"
 
   depends_on "go" => :build
